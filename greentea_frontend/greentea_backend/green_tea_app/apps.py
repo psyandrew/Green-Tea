a3bf82507474a3dedee3d618c075eb4d0a668d29
psyandrew <casero.andrew@gmail.com>
2025-06-11 15:23:29 +0800

    preparing for deployment

--- greentea_frontend/greentea_backend/green_tea_app/apps.py
+++
@@ -1,4 +0,0 @@
-from django.apps import AppConfig
-
-class GreenTeaConfig(AppConfig):
-    name = 'green_tea_app'
--- greentea_frontend/greentea_backend/green_tea_app/serializer.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Admin
-
-class AdminIdSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Admin
-        fields = ['id']  # Only serialize the 'id'
